cfda89b265a828eb939302a0f88ab80d620f768c
mnngy <59257197+mnngy@users.noreply.github.com>
2021-07-09 15:11:55 +0900

    유연한 컨트롤러2 - v5

--- src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public interface MyHandlerAdapter {
+public interface  MyHandlerAdapter {
 
     boolean supports(Object handler);
 
